d2bc6d6f701938c7367cec2df8b91c0fb2336542
Medha Adepu <medha.adepucse@gmail.com>
2026-01-27 18:43:44 +0530

    user management app

--- user-management-application/src/validations/validateUser.js
+++ user-management-application/src/validations/validateUser.js
@@ -1,6 +1,18 @@
 export const validateUser = ({ name, email, password, age, role }) => {
-  if (!name || !email || !password || !age || !role) {
-    return "Please fill all the fields";
+  if (!name?.trim()) {
+    return "Name is required";
+  }
+
+  if(!email.includes('@gmail.com')){
+    return "Email must be the right format"
+  }
+
+  if(!password || password.length < 8){
+    return "Password must be at least 8 characters"
+  }
+
+  if(age !== undefined && age < 18){
+    return "Age must be at least 18"
   }
   return null;
 };
